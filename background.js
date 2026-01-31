07ba009f2b27cdbd2adeb8ac6723e150f10e6ea8
yafiakhtar <yafi.7910@gmail.com>
2026-01-31 17:09:55 -0500

    fixed errors on extensions developer page

--- background.js
+++ background.js
@@ -1,30 +1,32 @@
 // Create context menu on install
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "highlight-text",
-    title: "Highlight",
-    contexts: ["selection"]
-  });
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: "highlight-text",
+      title: "Highlight",
+      contexts: ["selection"]
+    });
 
-  chrome.contextMenus.create({
-    id: "remove-highlight",
-    title: "Remove Highlight",
-    contexts: ["selection"]
+    chrome.contextMenus.create({
+      id: "remove-highlight",
+      title: "Remove Highlight",
+      contexts: ["selection"]
+    });
   });
 });
 
 // Handle context menu clicks
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "highlight-text") {
-    chrome.tabs.sendMessage(tab.id, { action: "highlight" });
+    chrome.tabs.sendMessage(tab.id, { action: "highlight" }).catch(() => {});
   } else if (info.menuItemId === "remove-highlight") {
-    chrome.tabs.sendMessage(tab.id, { action: "removeSelected" });
+    chrome.tabs.sendMessage(tab.id, { action: "removeSelected" }).catch(() => {});
   }
 });
 
 // Handle keyboard shortcut
 chrome.commands.onCommand.addListener((command, tab) => {
   if (command === "highlight-selection") {
-    chrome.tabs.sendMessage(tab.id, { action: "highlight" });
+    chrome.tabs.sendMessage(tab.id, { action: "highlight" }).catch(() => {});
   }
 });
